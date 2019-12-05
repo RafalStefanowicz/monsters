169e5f50297c1de25ba578bc8b8a85e5d0eea41b
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-12-05 21:02:12 +0100

    Add SpinnerIcon and styles to Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+import "./styles.css";
+import { SpinnerIcon } from "../Icons/SpinnerIcon";
+
 export const Loader = () => {
-  return <h2>Loading..</h2>;
+  return (
+    <h2 className="loader">
+      <SpinnerIcon />
+      Loading..
+    </h2>
+  );
 };
